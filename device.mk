894ac99bd78c03461abf56dce5321d8b05bfb3d2
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    onyx: Add libbt-vendor to products
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -122,7 +122,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # OnyxParts
 PRODUCT_PACKAGES += \
