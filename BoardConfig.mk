820edf0fe9b1ee3f04e0941cf576dd4506885a24
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    onyx: Add compatiblity matrix
    Change-Id: Ibf6053f1a7e9df5384664379772cb8757e897622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_onyx
