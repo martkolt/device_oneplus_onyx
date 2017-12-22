22fdfc3c2b746f79b232b2c554bf763eff97c5a2
Artem Borisov <dedsa2002@gmail.com>
2017-12-22 11:45:58 +0300

    onyx: Dex-preopt boot and system server
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,10 @@ BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT := true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
   endif
 endif
 
