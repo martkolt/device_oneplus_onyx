6684ffdb74d18a2f6c25e942547a212849f0d6fc
YumeMichi <do4suki@gmail.com>
2017-12-29 14:49:35 +0800

    onyx: Use SettingsLib for OnyxParts
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- OnyxParts/Android.mk
+++ OnyxParts/Android.mk
@@ -2,6 +2,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v14-preference \
+    android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v4
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := OnyxParts
 LOCAL_CERTIFICATE := platform
@@ -11,6 +19,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_DEX_PREOPT := false
+
+include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
 
