Thunderbottom/android_device_oneplus_onyx

onyx: libshims: Add camera parameters shim
Signed-off-by: YumeMichi <do4suki@gmail.com>

libshims/Android.mk
@@ -28,3 +28,12 @@ LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := CameraParameters.cpp
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libcamera_parameters_shim
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

martkolt/device_oneplus_onyx

libshims/Android.mk
@@ -29,3 +29,12 @@ LOCAL_MODULE := libshims_atomic
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := CameraParameters.cpp
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libshims_cameraparams
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)