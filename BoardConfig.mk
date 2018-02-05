d3caef56c0164dd9c25517b5c1e24ed332adf589
YumeMichi <do4suki@gmail.com>
2018-02-05 16:40:47 +0800

    onyx: recovery: Update for TWRP building
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,16 +161,10 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.full
 
-# TWRP Recovery -- uncomment to build
-#BOARD_SUPPRESS_SECURE_ERASE := true
-#DEVICE_RESOLUTION := 1080x1920
-#RECOVERY_SDCARD_ON_DATA := true
-#RECOVERY_VARIANT := twrp
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-#TW_EXTRA_LANGUAGES := true
-#TW_INCLUDE_CRYPTO := true
-#TW_NO_USB_STORAGE := true
-#TW_TARGET_USES_QCOM_BSP := true
+# TWRP Recovery
+ifeq ($(WITH_TWRP),true)
+include $(PLATFORM_PATH)/twrp.mk
+endif
 
 # RIL
 TARGET_RIL_VARIANT := caf
---
+++ twrp.mk
@@ -0,0 +1,11 @@
+# TWRP
+BOARD_HAS_NO_SELECT_BUTTON   := true
+BOARD_SUPPRESS_SECURE_ERASE  := true
+DEVICE_RESOLUTION            := 1080x1920
+RECOVERY_SDCARD_ON_DATA      := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+# TW_EXTRA_LANGUAGES           := true
+TW_INCLUDE_CRYPTO            := true
+TW_NO_USB_STORAGE            := true
+TW_TARGET_USES_QCOM_BSP      := true
+TW_THEME                     := portrait_hdpi
