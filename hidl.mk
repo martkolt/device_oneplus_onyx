6b82c620adbe12f95cad064e06e6f019d97c0700
YumeMichi <do4suki@gmail.com>
2018-02-07 18:05:16 +0800

    onyx: Switch to binderized lineage vibrator HAL
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -52,7 +52,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.lineage
 
 # Thermal
 PRODUCT_PACKAGES += \
