f74c9b0787ef6a58464d3f3db5fbefd0d9263b10
Paul Keith <javelinanddart@gmail.com>
2018-02-19 18:21:29 +0100

    onyx: Use sdFAT for exFAT
    Change-Id: I660c5b3008636a651a2812fe1739ff8a2ea845cb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,7 +134,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Extended filesystem support
-TARGET_EXFAT_DRIVER := exfat
+TARGET_EXFAT_DRIVER := sdfat
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
