94aa2bd229a0bdfcbb88db21acd4b3a2cdad7632
Kshitij Gupta <kshitijgm@gmail.com>
2018-03-02 19:34:43 +0530

    onyx: Import and adapt camera provider
    - Import from hardware/interfaces/camera/provider/2.4/default
    - Remove vndbinder usage
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -13,6 +13,7 @@ PRODUCT_PACKAGES += \
 # Legacy Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service.onyx \
     camera.device@1.0-impl
 
 # Power
