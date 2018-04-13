0ace97a53dad9fb537874ebc1c74e057125a8395
YumeMichi <do4suki@gmail.com>
2018-04-13 18:15:52 +0800

    onyx: Copy gps.conf to gps_debug.conf
    * This should fix GPS locking issue?
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -151,6 +151,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps_debug.conf \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
